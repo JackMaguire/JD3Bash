b735bc4651c192cb1d9c9896b37985c4ca30185d
Jack Maguire <jackmaguire1444@gmail.com>
2018-09-28 14:27:58 -0400

    Formatting Graph.java

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -6,26 +6,26 @@ public class Graph {
 
 	private ArrayList< Node > nodes_;
 	private ArrayList< Edge > edges_;
-	
+
 	public Graph() {
 		nodes_ = new ArrayList< Node >();
 		edges_ = new ArrayList< Edge >();
 	}
-	
+
 	public int getNumNodes() {
 		return nodes_.size();
 	}
 
-	public Node getNode( int index ) {//zero-indexed
+	public Node getNode( int index ) {// zero-indexed
 		return nodes_.get( index );
 	}
-	
+
 	public int getNumEdges() {
 		return edges_.size();
 	}
-	
+
 	public Edge getEdge( int index ) {
 		return edges_.get( index );
 	}
-	
+
 }
