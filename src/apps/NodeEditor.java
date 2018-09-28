4b1f3984dd017a4e11c44e6d163f976990b35737
Jack Maguire <jackmaguire1444@gmail.com>
2018-09-28 14:03:31 -0400

    Added main()

--- src/apps/NodeEditor.java
+++ src/apps/NodeEditor.java
@@ -1,9 +1,16 @@
 package apps;
 
+import javax.swing.JFrame;
+
 public class NodeEditor {
 
 	public static void main( String[] args ) {
-		
+		JFrame F = new JFrame( "RosettaNodeEditor" );
+		F.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		F.setExtendedState( JFrame.MAXIMIZED_BOTH );
+		//F.setUndecorated( true );
+		//F.add( new MainView() );
+		F.setVisible( true );
 	}
 	
 }
