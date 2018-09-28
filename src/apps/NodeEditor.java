a9547ffa22374f1d2f3e420ae01d85152ab6ed70
Jack Maguire <jackmaguire1444@gmail.com>
2018-09-28 14:18:58 -0400

    Formatting

--- src/apps/NodeEditor.java
+++ src/apps/NodeEditor.java
@@ -8,9 +8,9 @@ public class NodeEditor {
 		JFrame F = new JFrame( "RosettaNodeEditor" );
 		F.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		F.setExtendedState( JFrame.MAXIMIZED_BOTH );
-		//F.setUndecorated( true );
-		//F.add( new MainView() );
+		// F.setUndecorated( true );
+		// F.add( new MainView() );
 		F.setVisible( true );
 	}
-	
+
 }
--- src/graph/Node.java
+++ src/graph/Node.java
@@ -5,97 +5,97 @@ import java.util.ArrayList;
 public class Node {
 
 	private int id_;
-	
-	//GUI position
+
+	// GUI position
 	private int x_;
 	private int y_;
-	
-	private ArrayList< Edge > upstream_edges_;//Connecting to nodes that occur before this node
-	private ArrayList< Edge > downstream_edges_;//Connecting to nodes that occur after this node
+
+	private ArrayList< Edge > upstream_edges_;// Connecting to nodes that occur before this node
+	private ArrayList< Edge > downstream_edges_;// Connecting to nodes that occur after this node
 
 	private String command_ = "rosetta_scripts.mpi.linuxgccrelease @ flags";
-	
+
 	public Node( int id, int x, int y ) {
 		id_ = id;
 		x_ = x;
 		y_ = y;
-		
+
 		upstream_edges_ = new ArrayList< Edge >();
 		downstream_edges_ = new ArrayList< Edge >();
 	}
-	
+
 	///////////////////////
-	//Getters and Setters//
+	// Getters and Setters//
 	///////////////////////
-	
+
 	public final int id() {
 		return id_;
 	}
-	
+
 	public final void setId( int id ) {
 		id_ = id;
 	}
-	
+
 	public final int x() {
 		return x_;
 	}
-	
+
 	public final void setX( int x ) {
 		x_ = x;
 	}
-	
+
 	public final int y() {
 		return y_;
 	}
-	
+
 	public final void setY( int y ) {
 		y_ = y;
 	}
-	
+
 	public final void addUpstreamEdge( Edge e ) {
 		upstream_edges_.add( e );
 	}
-	
+
 	public final void removeUpstreamEdge( Edge e ) {
 		upstream_edges_.remove( e );
 	}
-	
+
 	public final int numUpstreamEdges() {
 		return upstream_edges_.size();
 	}
-	
+
 	public final Edge getUpstreamEdge( int i ) {
-		//ArrayList does error handling so we don't have to
+		// ArrayList does error handling so we don't have to
 		return upstream_edges_.get( i );
 	}
-	
+
 	public final void addDownstreamEdge( Edge e ) {
 		downstream_edges_.add( e );
 	}
-	
+
 	public final void removeDownstreamEdge( Edge e ) {
 		downstream_edges_.remove( e );
 	}
-	
+
 	public final int numDownstreamEdges() {
 		return downstream_edges_.size();
 	}
-	
+
 	public final Edge getDownstreamEdge( int i ) {
-		//ArrayList does error handling so we don't have to
+		// ArrayList does error handling so we don't have to
 		return downstream_edges_.get( i );
 	}
-	
+
 	public final String getCommand() {
 		return command_;
 	}
-	
+
 	public final String command() {
 		return command_;
 	}
-	
+
 	public final void setCommand( String setting ) {
 		command_ = setting;
 	}
-	
+
 }
