bef706189aae15940d95109102e91e203e1312f1
Jack Maguire <jackmaguire1444@gmail.com>
2018-09-28 14:23:46 -0400

    Filled out Edge setters and getters

--- src/graph/Edge.java
+++ src/graph/Edge.java
@@ -2,4 +2,31 @@ package graph;
 
 public class Edge {
 
+	private Node source_node_;
+	private Node destination_node_;
+	
+	public Edge() {
+		
+	}
+	
+	public Edge( Node source_node, Node dest_node ) {
+		source_node_ = source_node;
+		destination_node_ = dest_node;
+	}
+	
+	public final Node sourceNode() {
+		return source_node_;
+	}
+	
+	public final void setSourceNode( Node source_node ) {
+		source_node_ = source_node;
+	}
+	
+	public final Node destinationNode() {
+		return destination_node_;
+	}
+	
+	public final void setDestinationNode( Node destination_node ) {
+		destination_node_ = destination_node;
+	}
 }
