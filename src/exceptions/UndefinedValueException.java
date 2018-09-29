dd27107cc2afaaea3d86d95f14a446a1ffa9490f
Jack Maguire <jackmaguire1444@gmail.com>
2018-09-29 09:10:49 -0400

    UndefinedValueException

---
+++ src/exceptions/UndefinedValueException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class UndefinedValueException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9177376148390293941L;
+
+	public UndefinedValueException( String s ) {
+		super(s);
+	}
+	
+}
