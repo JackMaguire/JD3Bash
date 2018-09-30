f030a7b8d26f1ba0f310406aaa9775a9c47edbe6
Jack Maguire <jackmaguire1444@gmail.com>
2018-09-30 13:44:29 -0400

    Started work on Control Panel but let's do that later

---
+++ src/controllers/GraphViewControlPanelController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javax.swing.JButton;
+
+import views.GraphViewControlPanel;
+
+public class GraphViewControlPanelController {
+
+	private final GraphViewControlPanel gvc_panel_;
+	
+	private final JButton organize_graph_ = new JButton( "Organize Graph" );
+	private final JButton compile_button_ = new JButton( "Compile" );
+	
+	public GraphViewControlPanelController() {
+		gvc_panel_ = new GraphViewControlPanel();
+	}
+	
+}
