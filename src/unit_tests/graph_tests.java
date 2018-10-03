64d21d4416ec46f988b214dfe0b250b4a97b0d45
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-03 16:33:45 -0400

    Started unit test

--- src/unit_tests/graph_tests.java
+++ src/unit_tests/graph_tests.java
@@ -1,5 +1,8 @@
 package unit_tests;
 
+import java.awt.Color;
+
+import graph.Edge;
 import graph.Graph;
 import graph.Node;
 
@@ -13,6 +16,13 @@ public class graph_tests {
 			success = false;
 		} else {
 			System.out.println( "\ttest_SimpleGeometry() passed" );
+		}
+		
+		if( !test_SaveAndLoad() ) {
+			System.err.println( "\ttest_SaveAndLoad() failed" );
+			success = false;
+		} else {
+			System.out.println( "\ttest_SaveAndLoad() passed" );
 		}
 
 		return success;
@@ -32,8 +42,8 @@ public class graph_tests {
 
 			// 2 -> 0 -> 1
 
-			test_graph.addEdge( test_graph.getNode( 2 ), test_graph.getNode( 0 ) );
-			test_graph.addEdge( test_graph.getNode( 0 ), test_graph.getNode( 1 ) );
+			test_graph.addEdge( node2, node0 );
+			test_graph.addEdge( node0, node1 );
 
 			if( node0.numUpstreamEdges() != 1 ) {
 				System.err.println( "node0.numUpstreamEdges() == " + node0.numUpstreamEdges() + " instead of 1" );
@@ -64,14 +74,48 @@ public class graph_tests {
 				System.err.println( "node2.numDownstreamEdges() == " + node2.numDownstreamEdges() + " instead of 1" );
 				success = false;
 			}
-
-			if( !success ) {
-				// Don't do future tests if even these simple ones fail.
-				return false;
-			}
 		}
 
 		return success;
 	}// test_SimpleGeometry
 
+	
+	private final static boolean test_SaveAndLoad() {
+		boolean success = true;
+		
+		// 0->2
+		// 1->2
+		// 2->3
+		// 2->4
+		
+		final Graph original_graph = new Graph();
+		final Node[] nodes = new Node[ 4 ];
+		for( int i=0; i< nodes.length; ++i ) {
+			nodes[ i ] = new Node( i, i );
+			original_graph.addNode( nodes[ i ] );
+		}
+		
+		final Edge edge02 = original_graph.addEdge( nodes[ 0 ], nodes[ 2 ] );
+		final Edge edge12 = original_graph.addEdge( nodes[ 1 ], nodes[ 2 ] );
+		final Edge edge23 = original_graph.addEdge( nodes[ 2 ], nodes[ 3 ] );
+		final Edge edge24 = original_graph.addEdge( nodes[ 2 ], nodes[ 4 ] );
+		
+		edge02.setColumnNameToSortBy( "UNIT_TEST" );
+		edge12.setNotes( "UNIT\nTEST" );
+		edge23.setNumResultsToTransfer( 23 );
+		edge23.setPositiveScoresAreBetter( true );
+		edge24.setPercentageOfResultsToTransfer( 0.25 );
+		edge24.setUsePercentageInsteadOfCount( true );
+		
+		nodes[ 0 ].setColor( new Color( 1, 2, 3 ) );
+		nodes[ 1 ].setCommand( "../unit_test.sh" );
+		nodes[ 2 ].setNotes( "UNIT TEST" );
+		nodes[ 3 ].setScript( "unit_test.xml" );
+		nodes[ 4 ].setTitle( "unit_test" );
+		nodes[ 0 ].addUserRosettaFlag( "-unit test" );
+		nodes[ 1 ].setX( 100 );
+		nodes[ 1 ].setY( 200 );
+		
+		return success;
+	}
 }
