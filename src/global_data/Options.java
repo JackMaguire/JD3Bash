b7e7dac4707e180bc983b0f0920ba7e4ac017f88
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-04 09:07:19 -0400

    Added global options system (uh-oh)

---
+++ src/global_data/Options.java
@@ -0,0 +1,33 @@
+package global_data;
+
+public class Options {
+
+	//////////////
+	//View Options
+	private static boolean show_node_titles_ = false;
+	public static boolean getShowNodeTitles() {
+		return show_node_titles_;
+	}
+	protected static void setShowNodeTitles( boolean show_node_titles ) {
+		show_node_titles_ = show_node_titles;
+	}
+	
+	private static boolean put_node_titles_to_side_ = false;
+	public static boolean getPutNodeTitlesToSide() {
+		return put_node_titles_to_side_;
+	}
+	protected static void setPutNodeTitlesToSide( boolean put_node_titles_to_side ) {
+		put_node_titles_to_side_ = put_node_titles_to_side;
+	}
+	
+	
+	/////////////////
+	//Compile Options
+	private static boolean serialize_intermediate_poses_ = false;
+	public static boolean getSerializeIntermediatePoses() {
+		return serialize_intermediate_poses_;
+	}
+	protected static void setSerializeIntermediatePoses( boolean serialize_intermediate_poses ) {
+		serialize_intermediate_poses_ = serialize_intermediate_poses;
+	}
+}
