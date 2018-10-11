a7f718f5cb234a01ad5b4ada65749c09bcd61049
Jack Maguire <jackmaguire1444@gmail.com>
2018-10-11 10:00:11 -0400

    Formatting

--- src/apps/CreateFilesFromSaveFile.java
+++ src/apps/CreateFilesFromSaveFile.java
@@ -20,17 +20,17 @@ public class CreateFilesFromSaveFile {
 	private static String save_file = "";
 	private static String setup_out_file = "setup.sh";
 	private static String run_out_file = "run.sh";
-	
+
 	public static void main( String[] args ) {
-		
+
 		parseArgs( args );
 		if( save_file.length() == 0 ) {
 			System.err.println( "Please provide a save file using -s" );
 			System.exit( -1 );
 		}
-		
+
 		Graph graph = new Graph();
-		
+
 		BufferedReader in = null;
 		try {
 			in = new BufferedReader( new FileReader( save_file ) );
@@ -46,7 +46,8 @@ public class CreateFilesFromSaveFile {
 			System.exit( -1 );
 		}
 		catch( LoadFailureException e1 ) {
-			System.err.println( "Problem loading importing graph from " + save_file + "!\n" + e1.getMessage() );
+			System.err
+					.println( "Problem loading importing graph from " + save_file + "!\n" + e1.getMessage() );
 			System.exit( -1 );
 		}
 		finally {
@@ -59,7 +60,7 @@ public class CreateFilesFromSaveFile {
 				}
 			}
 		} // finally
-		
+
 		GraphParsingOptions options = new GraphParsingOptions();
 		options.serialize_intermediate_poses = global_data.Options.getSerializeIntermediatePoses();
 		Pair< String, String > run_and_setup = null;
@@ -67,13 +68,13 @@ public class CreateFilesFromSaveFile {
 			run_and_setup = GraphParsing.parseGraph( graph, options );
 		}
 		catch( InvalidGraphException e ) {
-			System.err.println( "Unable to parse graph\n" + e.getMessage()  );
+			System.err.println( "Unable to parse graph\n" + e.getMessage() );
 			e.printStackTrace();
 			System.exit( -1 );
 		}
 		String run_script = run_and_setup.first;
 		String setup_script = run_and_setup.second;
-		
+
 		try {
 			BufferedWriter out = new BufferedWriter( new FileWriter( setup_out_file ) );
 			out.write( setup_script + "\n" );
@@ -83,7 +84,7 @@ public class CreateFilesFromSaveFile {
 			System.err.println( "Unable to write to " + setup_script + "\n" + e.getMessage() );
 			e.printStackTrace();
 		}
-		
+
 		try {
 			BufferedWriter out = new BufferedWriter( new FileWriter( run_out_file ) );
 			out.write( run_script + "\n" );
@@ -94,40 +95,40 @@ public class CreateFilesFromSaveFile {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static void parseArgs( String[] args ) {
-		
-		for( int i=0; i<args.length; ++i ) {
-			
+
+		for( int i = 0; i < args.length; ++i ) {
+
 			if( args[ i ].equals( "-s" ) ) {
 				if( ++i == args.length ) {
 					System.err.println( "-s requires an option" );
 					System.exit( 1 );
 				}
-				
+
 				save_file = args[ i ];
 			}
-			
+
 			if( args[ i ].equals( "-setup" ) ) {
 				if( ++i == args.length ) {
 					System.err.println( "-setup requires an option" );
 					System.exit( 1 );
 				}
-				
+
 				setup_out_file = args[ i ];
 			}
-			
+
 			if( args[ i ].equals( "-run" ) ) {
 				if( ++i == args.length ) {
 					System.err.println( "-run requires an option" );
 					System.exit( 1 );
 				}
-				
+
 				run_out_file = args[ i ];
 			}
-			
+
 		}
-		
+
 	}
-	
+
 }
